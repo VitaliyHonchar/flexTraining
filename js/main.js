98a74ba914ea9b2f213c715d396e1d5228358119
Vitaliy Gonchar <VetalHonchar@gmail.com>
2018-11-10 21:56:25 +0200

    fix bugs
    fix bugs

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
-	$("#form1 .log").addClass("active");
-   	$("#form1 .reg").click(function(){
-   	$("#form2 .reg").addClass("active");
+	$("#form1 .reg").addClass("active");
+   	$("#form1 .log").click(function(){
+   	$("#form2 .log").addClass("active");
     $("#form1").toggleClass("active_form form_lost");
     $("#form2").toggleClass("form_hide active_form");
 });
-   $("#form2 .log").click(function(){
+   $("#form2 .reg").click(function(){
     $("#form2").toggleClass("active_form form_lost");
     $("#form1").toggleClass("form_lost active_form");
 	});
